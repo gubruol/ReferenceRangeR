04aa541761a581444c7fd25118d3f6336576d92f
mavouol <maike.voss@uni-oldenburg.de>
2025-08-03 19:41:36 +0200

    Update app.R

--- app.R
+++ app.R
@@ -654,7 +654,7 @@ server <- function(input, output, session) {
     dataframe$age[1:demosamplesize] <- round(runif(demosamplesize, min = 1, max = 99), digits = 2)
     dataframe$sex[1:demosamplesize] <- factor(sample(c("M", "F"), demosamplesize, replace = TRUE), levels = sexlist)
     dataframe$result[1:demosamplesize] <- SSlogis(dataframe$age[1:demosamplesize], 5, 60, 6) + rnorm(demosamplesize, mean = 37.5, sd = 3.75)
-    pathsize <- round(demosamplesize / 10, digits = 0) # 10% pathological values
+    pathsize <- round(demosamplesize / 100, digits = 0) # 10% pathological values
     dataframe$result[1:pathsize] <- SSlogis(dataframe$age[1:pathsize], 5, 60, 6) + rnorm(pathsize, 20, 5)
     dataframe$result[(1 + pathsize):(3 * pathsize)] <- SSlogis(dataframe$age[(1 + pathsize):(3 * pathsize)], 5, 60, 6) + rnorm(2 * pathsize, 28, 3)
     dataframe$result[(1 + 3 * pathsize):(8 * pathsize)] <- SSlogis(dataframe$age[(1 + 3 * pathsize):(8 * pathsize)], 5, 60, 6) + rnorm(5 * pathsize, 50, 4.5)
